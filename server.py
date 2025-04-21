701f4a5f4c81e1bb14d0bc4b59b5bf77e4888e20
Ava Penn <apenn2@caltech.edu>
2025-04-21 14:22:59 -0500

    adding some templates for the navbar

--- server.py
+++ server.py
@@ -4,20 +4,24 @@ from flask import Response, request, jsonify
 import json
 import re
 from datetime import datetime
+
 app = Flask(__name__)
 
 # ROUTES
 @app.route('/')
 def welcome():
-   return render_template()   
+    return render_template("welcome.html")   
 
-@app.route('/learn/<page_num>')
-def render_restaurant(page_num=None):
-    return render_template(f"p{page_num}.html")
+@app.route('/learn/<int:page_num>')
+def render_tutorial(page_num):
+    total_pages = 5
+    return render_template(f"learn{page_num}.html", page_num=page_num, total_pages=total_pages)
 
-@app.route('/quiz/<page_num>')
-def render_edit(id=None):
-    return render_template()
+@app.route('/quiz/<int:page_num>')
+def render_quiz(page_num):
+    total_pages = 10
+    return render_template(f"quiz{page_num}.html", page_num=page_num, total_pages=total_pages)
+
 
 if __name__ == '__main__':
-   app.run(debug = True, port=5001)
+    app.run(debug=True, port=5001)
